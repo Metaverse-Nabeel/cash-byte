2fec046dafb58fa039dde4a954fba6397a6c31d6
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-08-10 00:23:04 +0500

    Fix Linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    
-    protected
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up,
-        keys: [:username, :name, :email, :password, :password_confirmation, :remember_me])
-      devise_parameter_sanitizer.permit(:sign_in,
-        keys: [:login, :password, :password_confirmation])
-      devise_parameter_sanitizer.permit(:account_update,
-        keys: [:username, :name, :email, :password_confirmation, :current_password])
-    end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[username name email password password_confirmation remember_me])
+    devise_parameter_sanitizer.permit(:sign_in,
+                                      keys: %i[login password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[username name email password_confirmation current_password])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,28 +4,29 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, uniqueness: {case_sensitive: false}
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 
   validate :validate_username
 
   def validate_username
-    if User.where(email: username).exists?
-      errors.add(:username, :invalid)
-    end
+    return unless User.where(email: username).exists?
+
+    errors.add(:username, :invalid)
   end
 
   attr_writer :login
 
   def login
-    @login || self.username ||self.email
+    @login || username || email
   end
 
-  def self.find_for_database_authentication warden_condition
+  def self.find_for_database_authentication(warden_condition)
     conditions = warden_condition.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value",{ value: login.strip.downcase}]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value',
+                                    { value: login.strip.downcase }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
       where(conditions.to_h).first
-    end 
+    end
   end
 end
