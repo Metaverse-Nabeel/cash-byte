b405a1fc05ecef07ca5bff01c7e5ecc7ab4d2ff0
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-08-09 20:48:21 +0500

    User model made

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_09_075302) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_153755) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_075302) do
   create_table "tranxactions", force: :cascade do |t|
     t.string "name"
     t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
