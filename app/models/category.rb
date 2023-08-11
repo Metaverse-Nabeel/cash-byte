99c3e8e3d634b45c5c7b5cf66c352049c5894ce3
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-08-11 12:52:58 +0500

    Fix categories icon issue

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_one_attached :icon # For Active Storage
   has_and_belongs_to_many :deals
   validates :name, presence: true
   validates :icon, presence: true
