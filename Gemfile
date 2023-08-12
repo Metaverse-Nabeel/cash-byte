cc1918e505fd2f5be6250fdf158693c3dee26461
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-08-12 11:00:16 +0500

    Add test for the categories_spec as per reviewers remarks

--- Gemfile
+++ Gemfile
@@ -37,4 +37,5 @@ group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
   gem 'webdrivers'
+  gem 'database_cleaner'
 end
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -4,13 +4,24 @@ RSpec.describe 'Categories', type: :request do
   before :each do
     @user = User.create(name: 'Ben', email: 'ben997@gmail.com', password: 'benkis112')
 
+    # Create test images in the spec/fixtures directory and use them for attachments
+    icon_path_sports = Rails.root.join('spec', 'fixtures', 'sports.png')
+    icon_path_movies = Rails.root.join('spec', 'fixtures', 'movies.png')
+
     Category.create([
-                      { user: @user, name: 'Sports',
-                        icon: 'D:\Work\Microverse\Module 5\Week 5\cash-byte\app\assets\images\bike.png' },
-                      { user: @user, name: 'Movies',
-                        icon: 'D:\Work\Microverse\Module 5\Week 5\cash-byte\app\assets\images\car.png' }
-                    ])
+      { user: @user, name: 'Sports', icon: fixture_file_upload(icon_path_sports, 'image/png') },
+      { user: @user, name: 'Movies', icon: fixture_file_upload(icon_path_movies, 'image/png') }
+    ])
 
     get categories_path
   end
+
+  it 'for a renders index template' do
+    expect(response).to have_http_status(:found)
+  end
+
+  it 'for a success' do
+    expect(response).to have_http_status(:found)
+  end
+  
 end
